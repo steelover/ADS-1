8da087ce9565964cbe63dffe5f987f3ccd1c8f18
steelover <150594943+steelover@users.noreply.github.com>
2024-03-02 22:37:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,12 +4,9 @@
 
 
 bool checkPrime(uint64_t value) {
-	if (value == 1)
-		return false;
-	if ((value == 2) || (value == 3) || (value == 5))
-		return true;
-	if (!(value % 2))
-		return false;
+	if (value == 1) return false;
+	if ((value == 2) || (value == 3) || (value == 5)) return true;
+	if (!(value % 2)) return false;
 	else {
 		for (int i = 3; i < value / 2; i += 2) {
 			if (!(value / i))
@@ -22,36 +19,29 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
 	int s = 0;
 	for (int i = 1;; i++) {
-		if (checkPrime(i))
-			s++;
-		if (s == n)
-			return i;
+		if (checkPrime(i)) s++;
+		if (s == n) return i;
 	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-	if (value <= 1)
-		return nextPrime(value + 1);
+	if (value <= 1) return nextPrime(value + 1);
 	if (checkPrime(value)) {
 		int q = 0;
 		for (int j = 1;; j++) {
-			if (nPrime(j) == value)
-				q = j;
+			if (nPrime(j) == value) q = j;
 			return nPrime(q + 1);
 		}
 	}
-	else
-		return nextPrime(value - 1);
+	else return nextPrime(value - 1);
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	if (hbound <= 1)
-		return 0;
+	if (hbound <= 1) return 0;
 	else {
 		int summ = 0;
 		for (int i = 1; i < hbound - 1; i++) {
-			if (checkPrime(i))
-				summ += i;
+			if (checkPrime(i)) summ += i;
 		}
 		return summ;
 	}
